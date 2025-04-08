9c4835bb066e03e393cee47ae49bd7f680a68459
rakhirode06 <2022bit006@sggs.ac.in>
2025-04-08 18:19:35 +0530

    first commit

--- SGGS_20_09_21/settings.py
+++ SGGS_20_09_21/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+CSRF_TRUSTED_ORIGINS=["web-production-e2b62.up.railway.app"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
